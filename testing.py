b991bf2bb8b533df813e90723242d95578b2ef7b
qishuo <qs0727@qq.com>
2019-08-14 17:09:52 +0800

    #1 edit testing.py

--- testing.py
+++ testing.py
@@ -34,7 +34,7 @@ img_path = "./test_imgs/test01.jpg"
 bbox, face_square = get_face_detection_result(model_face, img_path, device,
                                               nms_threshold=0.3, detector_threshold=0.9, img_size=150)
 
-# --------------------结果--------------------
+# -----------------------结果-----------------------
 
 print(face_square)
 print(len(bbox))
